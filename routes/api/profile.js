c790f48e8dc64f93684d51a17e1269daf9435042
ashcbrd <cabardoash@gmail.com>
2021-10-07 14:36:36 +0800

    delete user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -123,6 +123,23 @@ router.get('/user/:user_id', async (req, res) => {
 
         res.status(500).send('Server Error');
     }
-})
+});
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - remove users posts
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'user deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 
 module.exports = router;

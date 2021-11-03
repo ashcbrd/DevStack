618048b6e7036c9631dc6b867b449a495a0169df
ashcbrd <cabardoash@gmail.com>
2021-11-03 22:46:08 +0800

    added link to navbar logo

--- client/src/components/constants/Navbar.jsx
+++ client/src/components/constants/Navbar.jsx
@@ -10,7 +10,10 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              <div className="fa fa-laptop-code h-8 w-8 text-white text-2xl " />
+              <Link
+                className="fa fa-laptop-code h-8 w-8 text-white text-2xl "
+                to="/"
+              />
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">

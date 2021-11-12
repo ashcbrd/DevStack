a016b18612ee9f307cc65cd0de44056af2dc2106
ashcbrd <cabardoash@gmail.com>
2021-11-13 03:02:11 +0800

    added alert message

--- client/src/components/layout/Alert.jsx
+++ client/src/components/layout/Alert.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const Alert = props => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Alert = ({ alerts }) =>
+  alerts !== null &&
+  alerts?.length > 0 &&
+  alerts.map((alert) => (
+    <div
+      key={alert.id}
+      className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+    >
+      {alert.msg}
+    </div>
+  ));
 
 Alert.propTypes = {
+  alerts: PropTypes.array.isRequired,
+};
 
-}
-
-export default Alert
+const mapStateToProps = (state) => ({
+  alerts: state.alert,
+});
+export default connect(mapStateToProps)(Alert);

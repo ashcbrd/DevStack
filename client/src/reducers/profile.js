3153e8787d4515ba239cbe62e942e0f832361f4d
ashcbrd <cabardoash@gmail.com>
2022-12-08 22:41:14 +0800

    added education and experience actions

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,9 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "actions/types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+} from "actions/types";
 
 const initialState = {
   profile: null,
@@ -14,6 +19,7 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,

ca40a7b40715695ee99f1f8a3b44b8c59a32337b
ashcbrd <cabardoash@gmail.com>
2022-12-08 22:41:44 +0800

    fixed alert position

--- client/src/components/layout/Alert.jsx
+++ client/src/components/layout/Alert.jsx
@@ -6,10 +6,10 @@ const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
   alerts.map((alert) => (
-    <div className="flex w-3/4">
+    <div className="w-3/4 m-auto">
       <div
         key={alert.id}
-        className="z-50 relative text-xs md:text-base -mb-5 bg-blue-100 border border-red-400 text-red-blue px-4 py-3 rounded mt-5 md:w-full md:mx-auto"
+        className="z-50 relative  text-xs md:text-base m-auto -mb-2 bg-blue-100 border border-red-400 text-red-blue px-4 py-3 rounded mt-5 md:w-full md:mx-auto"
       >
         {alert.msg}
       </div>

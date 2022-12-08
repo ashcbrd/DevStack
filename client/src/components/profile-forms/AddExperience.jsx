3049e25f9dd3d5c83b970f4ad06a74eca5d040de
ashcbrd <cabardoash@gmail.com>
2022-12-08 22:39:43 +0800

    add experience form

---
+++ client/src/components/profile-forms/AddExperience.jsx
@@ -0,0 +1,142 @@
+import React, { Fragment, useState } from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addExperience } from "actions/profile";
+
+const AddExperience = ({ addExperience, history }) => {
+  const [formData, setFormData] = useState({
+    company: "",
+    title: "",
+    location: "",
+    from: "",
+    to: "",
+    current: false,
+    description: "",
+  });
+
+  const [toDateDisabled, toggleDisabled] = useState(false);
+
+  const { company, title, location, from, to, current, description } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <Fragment>
+      <h1>Add Experience</h1>
+      <p>
+        <i className="fas fa-code-branch"></i> Add any developer experience
+      </p>
+      <div className="md:flex w-full md:w-1/2 items-center">
+        <form
+          className="flex flex-col mx-8 p-8 md:mx-auto md:w-full"
+          onSubmit={(e) => {
+            e.preventDefault();
+            addExperience(formData, history);
+          }}
+        >
+          <div>
+            <input
+              className="my-3 border-2 border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+              type="text"
+              placeholder="* Job Title"
+              name="title"
+              value={title}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <div>
+            <input
+              className="my-3 border-2 border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+              type="text"
+              placeholder="* Company"
+              name="company"
+              value={company}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <div>
+            <input
+              className="my-3 border-2 border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+              type="text"
+              placeholder="Location"
+              name="location"
+              value={location}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                From
+              </label>
+            </div>
+            <input
+              className="my-3 border-2 border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+              type="date"
+              name="from"
+              value={from}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3"></div>
+            <label className="md:w-2/3 block text-gray-500 font-bold">
+              <input
+                className="mr-2 leading-tigh"
+                type="checkbox"
+                checked={current}
+                value={current}
+                onChange={(e) => {
+                  setFormData({ ...formData, current: !current });
+                  toggleDisabled(!toDateDisabled);
+                }}
+              />
+              <span className="text-sm">Current</span>
+            </label>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                To
+              </label>
+            </div>
+            <input
+              className="my-3 border-2 border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+              type="date"
+              name="to"
+              value={to}
+              onChange={(e) => onChange(e)}
+              disabled={toDateDisabled ? "disabled" : ""}
+            />
+          </div>
+          <div>
+            <textarea
+              className="my-3 border-2 border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+              placeholder="Job Description"
+              name="description"
+              cols="30"
+              rows="5"
+              value={description}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <input
+            type="submit"
+            className="w-full my-3 text-white rounded-full shadow-base p-1 bg-blue-500 transition-all border-2 border-blue-500 hover:opacity-80 hover:shadow-lg"
+            value="Submit"
+          />
+        </form>
+      </div>
+    </Fragment>
+  );
+};
+
+AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addExperience })(AddExperience);
